eaaf5ebde97d6dd82ec313ed2dda5c00a7868195
Marlon Leerkotte <leerkotte123@gmail.com>
2017-04-23 19:24:24 +0200

    Added new functions, changed control # New functions - Get Banlist - Kick all clients ;) - Changed control from inlinekeyboard to inlinequery

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#
-# Basic example for a bot that uses inline keyboards.
-# This program is dedicated to the public domain under the CC0 license.
 
-import logging
+from uuid import uuid4
+
+import re
 import configparser
 from teamspeakActions import teamspeakActions
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
-
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+from telegram import InlineQueryResultArticle, ParseMode, \
+    InputTextMessageContent
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+import logging
 
 config = configparser.ConfigParser()
 config.sections()
@@ -23,44 +21,90 @@ TEAMSPEAK_QUERY_USER = config['Teamspeak']['TEAMSPEAK_QUERY_USER']
 TEAMSPEAK_QUERY_PASS = config['Teamspeak']['TEAMSPEAK_QUERY_PASS']
 TEAMSPEAK_QUERY_SERVER = config['Teamspeak']['TEAMSPEAK_QUERY_SERVER']
 
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments bot and
+# update. Error handlers also receive the raised TelegramError object in error.
 def start(bot, update):
-    keyboard = [[InlineKeyboardButton("TS Clients", callback_data='1')]]
-
-    reply_markup = InlineKeyboardMarkup(keyboard)
-
-    update.message.reply_text('Please choose:', reply_markup=reply_markup)
-
-
-def button(bot, update):
-    query = update.callback_query
-    teamspeakClients = teamspeakActions.getClients(TEAMSPEAK_QUERY_USER, TEAMSPEAK_QUERY_PASS, TEAMSPEAK_QUERY_SERVER)
-
-    bot.editMessageText(text="%s" % teamspeakClients,
-                        chat_id=query.message.chat_id,
-                        message_id=query.message.message_id,
-                        parse_mode=ParseMode.MARKDOWN)
+    update.message.reply_text('Hi!')
 
 
 def help(bot, update):
-    update.message.reply_text("Use /start to test this bot.")
+    update.message.reply_text('Help!')
+
+
+def escape_markdown(text):
+    """Helper function to escape telegram markup symbols"""
+    escape_chars = '\*_`\['
+    return re.sub(r'([%s])' % escape_chars, r'\\\1', text)
+
+
+def inlinequery(bot, update):
+    query = update.inline_query.query
+
+    results = list()
+
+    results.append(InlineQueryResultArticle(id=uuid4(),
+                                            title="Get Clients",
+                                            input_message_content=InputTextMessageContent(
+                                                teamspeakActions.getClients(TEAMSPEAK_QUERY_USER, TEAMSPEAK_QUERY_PASS,
+                                                                            TEAMSPEAK_QUERY_SERVER),
+                                                parse_mode=ParseMode.MARKDOWN)))
+
+    results.append(InlineQueryResultArticle(id=uuid4(),
+                                            title="Get Banlist",
+                                            input_message_content=InputTextMessageContent(
+                                                teamspeakActions.getBanlist(TEAMSPEAK_QUERY_USER, TEAMSPEAK_QUERY_PASS,
+                                                                            TEAMSPEAK_QUERY_SERVER),
+                                                parse_mode=ParseMode.MARKDOWN)))
+
+    results.append(InlineQueryResultArticle(id=uuid4(),
+                                            title="Kick all clients",
+                                            input_message_content=InputTextMessageContent(
+                                                "*He'j het verstaand in'n tuk zit'n ofzo? Grapjas*",
+                                                parse_mode=ParseMode.MARKDOWN)))
+
+    update.inline_query.answer(results, cache_time=0)
 
 
 def error(bot, update, error):
-    logging.warning('Update "%s" caused error "%s"' % (update, error))
+    logger.warn('Update "%s" caused error "%s"' % (update, error))
 
 
-# Create the Updater and pass it your bot's token.
-updater = Updater(token=TELEGRAM_API_TOKEN)
+def main():
+    # Create the Updater and pass it your bot's token.
+    updater = Updater(TELEGRAM_API_TOKEN)
 
-updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CallbackQueryHandler(button))
-updater.dispatcher.add_handler(CommandHandler('help', help))
-updater.dispatcher.add_error_handler(error)
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
 
-# Start the Bot
-updater.start_polling()
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
 
-# Run the bot until the user presses Ctrl-C or the process receives SIGINT,
-# SIGTERM or SIGABRT
-updater.idle()
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(InlineQueryHandler(inlinequery))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Block until the user presses Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+def translate(m_string, char):
+    return m_string.translate(None, "i")
+
+
+if __name__ == '__main__':
+    main()
--- teamspeakActions.py
+++ teamspeakActions.py
@@ -23,10 +23,44 @@ class teamspeakActions:
             # print("Clients on the server:", resp.parsed)
             # print("Error:", resp.error["id"], resp.error["msg"])
 
-            teamspeakClients = "*Online Pornstars*\n"
+            teamspeakClients = "*Online Clients*\n"
 
             for client in resp.parsed:
                 if client['client_type'] == '0':
                     teamspeakClients = teamspeakClients + "_" + client['client_nickname'] + "_\n"
 
             return teamspeakClients
+
+    def getBanlist(TEAMSPEAK_QUERY_USER, TEAMSPEAK_QUERY_PASS, TEAMSPEAK_QUERY_SERVER):
+        with ts3.query.TS3Connection(TEAMSPEAK_QUERY_SERVER) as ts3conn:
+            # Note, that the client will wait for the response and raise a
+            # **TS3QueryError** if the error id of the response is not 0.
+            try:
+                ts3conn.login(
+                    client_login_name=TEAMSPEAK_QUERY_USER,
+                    client_login_password=TEAMSPEAK_QUERY_PASS
+                )
+            except ts3.query.TS3QueryError as err:
+                print("Login failed:", err.resp.error["msg"])
+                exit(1)
+
+            ts3conn.use(sid=1)
+
+            # Each query method will return a **TS3QueryResponse** instance,
+            # with the response.
+            try:
+                resp = ts3conn.banlist()
+                teamspeakBanlist = "*Banned Clients*\n"
+
+                for ban in resp.parsed:
+                    ip = ban['ip']
+                    ip = ip.translate({ord(c): None for c in '\\'})
+
+                    teamspeakBanlist = teamspeakBanlist + "_Name: " + ban['name'] + " IP: " + ip + \
+                                       " By: " + ban['invokername'] + " Reason: " + ban['reason'] + "_\n"
+
+                response = teamspeakBanlist
+            except ts3.query.TS3QueryError as err:
+                response = "No active bans."
+
+            return response
